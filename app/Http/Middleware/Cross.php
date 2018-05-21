a7489add2104e5579d49cc3231672457db24014c
wang zaron <wangzaron@env.pinehub>
2018-05-21 15:54:03 +0800

    update config

--- app/Http/Middleware/Cross.php
+++ app/Http/Middleware/Cross.php
@@ -17,13 +17,15 @@ class Cross
      */
     public function handle($request, Closure $next = null)
     {
-        if(strtoupper($request->method()) === "OPTIONS"){
-            $response = app('api.http.response')->noContent()->setStatusCode(HTTP_STATUS_NO_CONTENT);
-            $this->setHeader($response);
-        }else{
-            $response = $next($request);
-            $this->setHeader($response);
-        }
+//        if(strtoupper($request->method()) === "OPTIONS"){
+//            $response = app('api.http.response')->noContent()->setStatusCode(HTTP_STATUS_NO_CONTENT);
+//            $this->setHeader($response);
+//        }else{
+//            $response = $next($request);
+//            $this->setHeader($response);
+//        }
+        $response = $next($request);
+        $this->setHeader($response);
 
         return $response;
     }
@@ -31,9 +33,11 @@ class Cross
     private function setHeader( &$response ) {
         if(!($response instanceof RedirectResponse)) {
             Log::debug('set header');
-            return $response->header('Access-Control-Allow-Origin', '*')
-                ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Cookie')
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Origin', '*');
+            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
+            $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+            $response->header('Access-Control-Allow-Credentials', 'true');
+            return $response;
         }else{
             return $response;
         }
