73a94e575509d36795ded267876117e513bdde1e
wang zaron <wangzaron@env.pinehub>
2018-05-21 20:09:51 +0800

    signed wechat pay

--- resources/views/payment/aggregate/wechatpay.blade.php
+++ resources/views/payment/aggregate/wechatpay.blade.php
@@ -326,6 +326,14 @@
                     wx.config({!! $config !!});
                     wx.ready(function () {
                         console.log('wx-sdk is ready to call the api');
+                        wx.checkJsApi({
+                            jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+                            success: function(res) {
+                                console.log(res);
+                                // 以键值对的形式返回，可用的api值true，不可用为false
+                                // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+                            }
+                        });
                     });
                 },
                 success:function(data) {
