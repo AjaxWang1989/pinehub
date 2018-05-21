dcaec25aad3f3623073e3b6ff8be75bb59ac32c7
wang zaron <wangzaron@env.pinehub>
2018-05-21 21:48:15 +0800

    ali pay

--- config/ali.php
+++ config/ali.php
@@ -14,15 +14,15 @@ return [
 
         'app_id'                    => env('ALI_PAYMENT_APPID', '2088131026450554'),
         'sign_type'                 => env('ALI_PAYMENT_SIGN_TYPE', 'RSA2'),// RSA  RSA2
-        'md5_key'                   => env('ALI_PAYMENT_MD5_KEY', ''),
+        'md5_key'                   => env('ALI_PAYMENT_MD5_KEY', 'zismisutln32vdlavqdg7brqmya9z3ts'),
 
         // ！！！注意：如果是文件方式，文件中只保留字符串，不要留下 -----BEGIN PUBLIC KEY----- 这种标记
         // 可以填写文件路径，或者密钥字符串  当前字符串是 rsa2 的支付宝公钥(开放平台获取)
-        'ali_public_key'            => storage_path(env('ALI_PAYMENT_PUBLIC_KEY_PATH', '/ali/payment/publicKey')),
+        'ali_public_key'            => storage_path(env('ALI_PAYMENT_PUBLIC_KEY_PATH', '/private/ali/rsa_public_key.pem')),
 
         // ！！！注意：如果是文件方式，文件中只保留字符串，不要留下 -----BEGIN RSA PRIVATE KEY----- 这种标记
         // 可以填写文件路径，或者密钥字符串  我的沙箱模式，rsa与rsa2的私钥相同，为了方便测试
-        'rsa_private_key'           => storage_path(env('ALI_PAYMENT_PRIVATE_KEY_PATH', '/ali/payment/privateKey')),
+        'rsa_private_key'           => storage_path(env('ALI_PAYMENT_PRIVATE_KEY_PATH', '/private/ali/rsa_private_key.pem')),
         'limit_pay'                 => [
             //'balance',// 余额
             //'moneyFund',// 余额宝
@@ -35,7 +35,7 @@ return [
 
         // 与业务相关参数
         'notify_url'                => webUriGenerator(env('ALI_PAYMENT_NOTIFY_URL', '/payment/ali//notify')),
-        'return_url'                => webUriGenerator(env('ALI_PAYMENT_RETURN_URL', '/ali/payment/success')),
+        'return_url'                => webUriGenerator(env('ALI_PAYMENT_RETURN_URL', '/payment/ali/success')),
 
         'return_raw'                => env('ALI_PAYMENT_RETURN_RAW', false),// 在处理回调时，是否直接返回原始数据，默认为 true
     ],
