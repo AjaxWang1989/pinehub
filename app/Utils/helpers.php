f8e6920c89aeda8b8a7f3d4bf228b539372b29c0
wang zaron <wangzaron@env.pinehub>
2018-05-22 14:13:41 +0800

    fix

--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -121,6 +121,7 @@ if(!function_exists('webUriGenerator')) {
         if(substr($prefix, 0, 1) !== '/'){
             $prefix = '/'.$prefix;
         }
+        \Illuminate\Support\Facades\Log::debug(config('app'));
         return config('app.protocol').config('app.web_domain').$prefix.$route;
     }
 }
