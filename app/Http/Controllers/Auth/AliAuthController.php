aa10f2204b0fcc083a0d8de832fc9b4c31caffd6
wang zaron <wangzaron@env.pinehub>
2018-05-23 11:16:16 +0800

    fix

--- app/Http/Controllers/Auth/AliAuthController.php
+++ app/Http/Controllers/Auth/AliAuthController.php
@@ -5,17 +5,21 @@ namespace App\Http\Controllers\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Session;
 
 class AliAuthController extends Controller
 {
     //
     public function oauth2(Request $request)
     {
+        $token = Session::get('ali.oauth.token', null);
+        if($token){
+            Log::debug('session ali.oauth.token', $token);
+        }
         $authCode = $request->input('auth_code', null);
         $redirect = $request->input('redirect_uri', null);
-        Log::debug('ali auth code '.$authCode);
         $token = app('ali.oauth.token')->charge(['grant_type' => 'authorization_code', 'code' => $authCode]);
-        Log::debug('ali token', $token);
+        Session::put('ali.oauth.token', $token);
         return view('404');
     }
 }
