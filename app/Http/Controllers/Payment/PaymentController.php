ec9d39a044e3b14abe60cafef0598fe4012bbcf8
wang zaron <wangzaron@wangs-iMac.local>
2018-06-14 14:31:54 +0800

    fix

--- app/Http/Controllers/Payment/PaymentController.php
+++ app/Http/Controllers/Payment/PaymentController.php
@@ -51,7 +51,7 @@ class PaymentController extends Controller
         $this->orderModel = $orderRepositoryEloquent;
         $this->shopModel = $shopRepositoryEloquent;
         $this->app = $app;
-        $this->session = app('session');
+        $this->session = $app->has('session') ?  app('session') : null;
     }
 
     /**
