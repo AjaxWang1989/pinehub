78011eff2f3a54143144f68e92da98db0a6fcdd3
wang zaron <wangzaron@env.pinehub>
2018-06-28 21:14:53 +0800

    ip visit site forbeden

--- database/migrations/2018_06_05_092003_create_wechat_apps.php
+++ database/migrations/2018_06_05_092003_create_wechat_apps.php
@@ -17,7 +17,7 @@ class CreateWechatApps extends Migration
         Schema::create('wechat_apps', function (Blueprint $table) {
             $table->string('wechat_app_id')->comment('微信应用');
             $table->string('app_id')->comment('系统程序Id');
-            $table->unique(['wechat_app_id', 'app_slug']);
+            $table->unique(['wechat_app_id', 'app_id']);
         });
     }
 
