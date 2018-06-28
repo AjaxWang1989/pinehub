6294661b8b69ce1133e3f0485619ddd443a86efb
wang zaron <wangzaron@env.pinehub>
2018-06-28 20:02:30 +0800

    open platform auth

--- config/wechat.php
+++ config/wechat.php
@@ -72,7 +72,7 @@ return [
              */
              'oauth' => [
                  'scopes'   => array_map('trim', explode(',', env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_SCOPES', 'snsapi_userinfo'))),
-                 'callback' => env('WECHAT_OFFICIAL_ACCOUNT_OAUTH_CALLBACK', '/examples/oauth_callback.php'),
+                 'callback' => webUriGenerator(env('WECHAT_OPEN_PLATFORM_OAUTH_CALLBACK', '/examples/oauth_callback.php')),
              ],
         ],
     ],
