8fc7d2088703e7c24fb2ee37a8f3ee1b8cde99e4
wang zaron <wangzaron@wangs-iMac.local>
2018-07-05 21:35:42 +0800

    fix

--- app/Http/Controllers/Wechat/OpenPlatformController.php
+++ app/Http/Controllers/Wechat/OpenPlatformController.php
@@ -117,8 +117,8 @@ class OpenPlatformController extends Controller
 
     public function openPlatformAuthMakeSure(Request $request)
     {
-        return $this->response(new JsonResponse(['dd' => 'dd']));
         $auth = Cache::get(CURRENT_APP_PREFIX.$request->input('token'), false);
+        dd($auth);
         if($auth) {
             //$app = $this->appRepository->find($request->input('app_id'));
             return $this->response(new JsonResponse($auth));
