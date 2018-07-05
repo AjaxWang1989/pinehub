1687613df86ac75b8cac18abb9df1ad91ffe5a69
wang zaron <wangzaron@wangs-iMac.local>
2018-07-05 21:27:16 +0800

    fix

--- app/Http/Controllers/Wechat/OpenPlatformController.php
+++ app/Http/Controllers/Wechat/OpenPlatformController.php
@@ -123,7 +123,7 @@ class OpenPlatformController extends Controller
             //$app = $this->appRepository->find($request->input('app_id'));
             return $this->response(new JsonResponse($auth));
         }
-        $this->response()->error('未完成授权，请等待', HTTP_STATUS_NO_RESPONSE);
+        $this->response()->error('未完成授权，请等待', HTTP_STATUS_INTERNAL_SERVER_ERROR);
     }
 
     public function __destruct()
