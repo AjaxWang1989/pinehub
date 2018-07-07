3f63699d841280b587b30d724d8439c56dde6884
wang zaron <wangzaron@env.pinehub>
2018-07-07 19:56:50 +0800

    fix card type

--- app/Transformers/CardItemTransformer.php
+++ app/Transformers/CardItemTransformer.php
@@ -22,7 +22,7 @@ class CardItemTransformer extends TransformerAbstract
      */
     public function transform(CardItem $model)
     {
-        dump(isset($model->cardInfo['advanced_info']),
+        dump(isset($model->cardInfo),
             isset($model->cardInfo['reduce_cost']) ? $model->cardInfo['reduce_cost'] : null,
             isset($model->cardInfo['discount']),
             isset($model->cardInfo['discount']) ? $model->cardInfo['discount'] : null);
