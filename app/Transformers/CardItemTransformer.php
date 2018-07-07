a8a6039ddfb174ab100aa5a4a243ad7daab66221
wang zaron <wangzaron@env.pinehub>
2018-07-07 19:41:06 +0800

    fix card type

--- app/Transformers/CardItemTransformer.php
+++ app/Transformers/CardItemTransformer.php
@@ -22,6 +22,7 @@ class CardItemTransformer extends TransformerAbstract
      */
     public function transform(CardItem $model)
     {
+        dd($model->cardInfo);
         \Log::debug('card info', [is_array($model->cardInfo),  'discount' => isset($model->cardInfo['discount']) ]);
         return [
             'id'         => (int) $model->id,
