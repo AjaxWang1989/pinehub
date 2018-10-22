cff1aa9c856ca3575f68bfcabd816fb36d0a8dc0
wong1989 <skyandsunning@163.com>
2018-10-22 17:17:53 +0800

    fix

--- app/Http/Middleware/Cross.php
+++ app/Http/Middleware/Cross.php
@@ -27,16 +27,16 @@ class Cross
     }
 
     private function setHeader( &$response, $request) {
-
+        $origin = $request->header('ORIGIN', '*');
         if(method_exists($response, 'header')) {
-            $origin = $request->header('ORIGIN', '*');
             $response->header('Access-Control-Allow-Origin', $origin);
             $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
             $response->header('Access-Control-Allow-Credentials', 'true');
             return $response;
         }else{
-            header('Access-Control-Allow-Origin:*');
+
+            header('Access-Control-Allow-Origin:'.$origin);
             header('Access-Control-Allow-Headers:Origin, Content-Type, Cookie, Accept');
             header('Access-Control-Allow-Methods:GET, POST, PATCH, PUT, DELETE, OPTIONS');
             header('Access-Control-Allow-Credentials:true');
