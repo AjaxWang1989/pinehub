859932571644ed1ef85d193b822257df04d19867
wong1989 <skyandsunning@163.com>
2018-10-23 16:28:50 +0800

    修改

--- app/Transformers/AppItemTransformer.php
+++ app/Transformers/AppItemTransformer.php
@@ -23,7 +23,7 @@ class AppItemTransformer extends TransformerAbstract
      */
     public function transform(AppItem $model)
     {
-        dump(JWTAuth::getToken());
+        dump(app('tymon.jwt.auth')->getToken());
         return [
             'id'         => $model->id,
             'name' => $model->name,
@@ -33,7 +33,7 @@ class AppItemTransformer extends TransformerAbstract
             'logo' => $model->logo,
             /* place your other model properties here */
             'open_platform_auth_url' => buildUrl('web.wxopen', 'auth', [], [
-                'app_id' => $model->id, 'token' =>  app('tymon.jwt.auth')->getToken(), 'type' => 'all'
+                'app_id' => $model->id, 'token' =>  JWTAuth::getToken(), 'type' => 'all'
             ]),
             'created_at' => $model->createdAt,
             'updated_at' => $model->updatedAt
