a4366db98699e01adb0d6a6dd426097fa3f6d876
wong1989 <skyandsunning@163.com>
2018-10-23 21:27:42 +0800

    登陆token过期时间

--- app/Services/Wechat/WechatService.php
+++ app/Services/Wechat/WechatService.php
@@ -136,6 +136,7 @@ class WechatService
     {
         $callback = $this->openPlatform()->config['oauth']['callback'];
         $redirect = $callback(['appId' => $appId], ['token' => $token]);
+        dump($redirect);
         $url = $this->openPlatform()->getPreAuthorizationUrl($redirect);
         if($type) {
             switch ($type) {
