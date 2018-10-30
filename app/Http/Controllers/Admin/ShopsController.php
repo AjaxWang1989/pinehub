b237a0a6ab17998c28d6e6b568b6f36fb134ed0f
wong1989 <skyandsunning@163.com>
2018-10-31 04:02:18 +0800

    修改店铺详情接口

--- app/Http/Controllers/Admin/ShopsController.php
+++ app/Http/Controllers/Admin/ShopsController.php
@@ -101,7 +101,7 @@ class ShopsController extends Controller
                     return $query->whereNotNull('status');
                 }])
             ->paginate($request->input('limit', PAGE_LIMIT));
-//        return $shops;
+        return $shops;
         return $this->response()->paginator($shops, new ShopItemTransformer());
     }
 
