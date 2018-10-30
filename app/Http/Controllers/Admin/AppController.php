e95aebba2f3639f884f34bb7e79b54a269cfec24
wong1989 <skyandsunning@163.com>
2018-10-30 14:49:41 +0800

    fix

--- app/Http/Controllers/Admin/AppController.php
+++ app/Http/Controllers/Admin/AppController.php
@@ -144,11 +144,12 @@ class AppController extends Controller
     public function sevenDaysStatistics()
     {
         $end = Carbon::now();
-        $start = $end->copy()->subDay(7);
+        $start = $end->copy()->startOfWeek()->subDay(7);
         $project = app(AppManager::class)->currentApp;
-        $result = $project->orders()->select([DB::raw('count(*)'), DB::raw('DATE_FORMAT(`paid_at`, "%Y-%m-%d") as paid_time')])
+        $result = $project->orders()->select([DB::raw('count(*)'), DB::raw('DATE_FORMAT(`paid_at`, "%w") as paid_time')])
             ->where('paid_at', '>=', $start)
             ->where('paid_at', '<', $end)
+            ->groupBy('paid_time')
             ->get();
         return $result;
     }
