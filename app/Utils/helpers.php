8c16d6c06a13931a4668103525aecde190cdfbfe
wong1989 <skyandsunning@163.com>
2018-11-01 12:35:31 +0800

    fix

--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -259,7 +259,8 @@ if(!function_exists('is_assoc')) {
     function is_assoc($array) {
         if(is_array($array)) {
             $keys = array_keys($array);
-            return $keys !== array_keys($keys);
+            $diff = array_diff($keys, array_keys($keys));
+            return $diff && count($diff);
         }
         return false;
     }
