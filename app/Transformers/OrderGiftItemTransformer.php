173f03780f895f34f2b0ae0f8e0deccacf68495a
ZoranWong <904110231@qq.com>
2018-11-02 13:30:04 +0800

    修改支付活动

--- app/Transformers/OrderGiftItemTransformer.php
+++ app/Transformers/OrderGiftItemTransformer.php
@@ -30,7 +30,7 @@ class OrderGiftItemTransformer extends TransformerAbstract
             'status'     => $model->status,
             'order_count' => $model->orderCount,
             'customer_count' => $model->customerCount,
-            'money'      =>    $model->paymentAmount
+            'paid_amount'      =>    $model->paymentAmount
         ];
     }
 }
