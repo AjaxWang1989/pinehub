70029846da2a5fbdd2d2bd839de0b02bbdabf1a5
dwpweb <1357983268@qq.com>
2018-11-09 00:45:16 +0800

    添加重新支付的订单接口

--- app/Services/PaymentNotify.php
+++ app/Services/PaymentNotify.php
@@ -25,33 +25,44 @@ class PaymentNotify implements PayNotifyInterface
         $this->order = $model;
     }
 
-    public function notifyProcess(array $data)
+    /**
+     * @param array $data
+     * @throws \Exception
+     */
+    public function notifyProcess( array $data)
     {
         // TODO: Implement notifyProcess() method.
-        $order = $this->order->findWhere(['code' => $data['order_no']])->first();
-        $this->offLinePayOrder($order);
-        $order->transactionId = $data['transaction_id'];
-        if($data['trade_state'] === 'success'){
-            $order->tradeStatus = Order::TRADE_SUCCESS;
-            $order->status = Order::PAID;
-            $order->paidAt = Carbon::now();
-        } else {
-            $order->status = Order::PAY_FAILED;
-            $order->tradeStatus = Order::TRADE_FAILED;
-        }
-        tap($order, function (Order $order)use($data){
-            $result = $order->save();
-            if($result) {
-                //发送模版消息
-                $orderData=[];
-                if($order->type !== Order::OFF_LINE_PAY){
-                    $orderData['signed_at'] = $order->signedAt;
-                    $orderData['consigned_at'] = $order->consignedAt;
-                }
-                $orderData['status'] = $order->status;
-                $order->orderItems()->update($orderData);
+        Log::info('============wechat payment notify ============', $data);
+        /** @var TYPE_NAME $exception */
+        try{
+            $order = $this->order->findWhere(['code' => $data['order_no']])->first();
+            $this->offLinePayOrder($order);
+            $order->transactionId = $data['transaction_id'];
+            if($data['trade_state'] === 'success'){
+                $order->tradeStatus = Order::TRADE_SUCCESS;
+                $order->status = Order::PAID;
+                $order->paidAt = Carbon::now();
+            } else {
+                $order->status = Order::PAY_FAILED;
+                $order->tradeStatus = Order::TRADE_FAILED;
             }
-        });
+            tap($order, function (Order $order)use($data){
+                $result = $order->save();
+                if($result) {
+                    //发送模版消息
+                    $orderData=[];
+                    if($order->type !== Order::OFF_LINE_PAY){
+                        $orderData['signed_at'] = $order->signedAt;
+                        $orderData['consigned_at'] = $order->consignedAt;
+                    }
+                    $orderData['status'] = $order->status;
+                    $order->orderItems()->update($orderData);
+                }
+            });
+        }catch (\Exception $exception) {
+            throw $exception;
+        }
+
     }
 
     protected function offLinePayOrder(Order &$order)
