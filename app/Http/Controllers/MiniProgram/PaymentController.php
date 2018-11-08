95a9f14540ba6d436d88283da0286ef4fc53b80e
dwpweb <1357983268@qq.com>
2018-11-09 00:56:48 +0800

    添加重新支付的订单接口

--- app/Http/Controllers/MiniProgram/PaymentController.php
+++ app/Http/Controllers/MiniProgram/PaymentController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers\MiniProgram;
 
+use Dingo\Api\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Payment\NotifyContext;
 
 class PaymentController extends Controller
 {
 
-    public function notify()
+    public function notify(Request $request)
     {
+        Log::info('--------------------- payment notify------------------------------------',
+            $request->all());
         $notify = app('payment.wechat.notify');
         $data = $notify->notify($this->app->make('payment.notify'));
         return $this->response($data);
