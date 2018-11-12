4b094342d8009eaf58a8ec30b232e49a35b05714
ZoranWong <904110231@qq.com>
2018-11-12 16:48:20 +0800

    新品活动

--- app/Http/Requests/Admin/NewActivityMerchandiseRequest.php
+++ app/Http/Requests/Admin/NewActivityMerchandiseRequest.php
@@ -14,7 +14,7 @@ class NewActivityMerchandiseRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Admin/NewActivityRequest.php
+++ app/Http/Requests/Admin/NewActivityRequest.php
@@ -14,7 +14,7 @@ class NewActivityRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
