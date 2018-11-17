8c998ea2d89ff60fb6836bdff5a74795d1f2b82c
dwpweb <1357983268@qq.com>
2018-11-18 04:16:13 +0800

    优惠券接口

--- app/Repositories/CustomerTicketCardRepositoryEloquent.php
+++ app/Repositories/CustomerTicketCardRepositoryEloquent.php
@@ -76,14 +76,8 @@ class CustomerTicketCardRepositoryEloquent extends BaseRepository implements Cus
     {
         $this->scopeQuery(function (CustomerTicketCard $customerTicketCard) use($userId,$status) {
             return $customerTicketCard
-                ->where(['customer_id'=>$userId, 'customer_ticket_cards.status'=>$status])
-                ->join('cards', 'customer_ticket_cards.card_id', '=', 'cards.card_id')
-                ->with(['card' => function ($card) use ($userId){
-                    return $card->withCount(['records as record_count' => function ($records) use ($userId) {
-                        return $records->where('customer_id' , $userId);
-                    }]);
-                }])
-                ->groupBy('customer_ticket_cards.card_id');
+                ->where(['customer_id'=>$userId, 'status'=>$status])
+                ->groupBy('card_id');
         });
         return $this->paginate();
     }
