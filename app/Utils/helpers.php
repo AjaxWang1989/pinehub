2bcf43140db3dd505bf448135b3185150086cac2
wong1989 <skyandsunning@163.com>
2018-11-30 12:07:49 +0800

    fix

--- app/Utils/helpers.php
+++ app/Utils/helpers.php
@@ -265,3 +265,10 @@ if(!function_exists('is_assoc')) {
         return false;
     }
 }
+
+if ( ! function_exists('config_path')) {
+    /* Get the configuration path. @param string $path @return string */
+    function config_path($path = '') {
+    return app()->basePath() . '/config' . ($path ? '/' . $path : $path);
+    }
+}
