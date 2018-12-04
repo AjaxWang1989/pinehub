65bfcc882974b4a0fc32c929b17b59a091b3ba49
ZoranWong <904110231@qq.com>
2018-12-04 22:47:28 +0800

    优化小程序接口

--- app/Http/Requests/MiniProgram/StoreBuffetOrdersRequest.php
+++ app/Http/Requests/MiniProgram/StoreBuffetOrdersRequest.php
@@ -30,16 +30,14 @@ class StoreBuffetOrdersRequest extends FormRequest
     public function rules()
     {
         return [
-            'start_at' => 'required|date',
-            'end_at'   => 'required|date',
+            'paid_date' => 'required|date'
         ];
     }
 
     public function messages()
     {
         return [
-            'start_at.required' => '支付开始时间不能为空且时间格式要为date',
-            'end_at.required'   => '支付结束时间不能为空且时间格式要为date',
+            'paid_date.required' => '支付开始时间不能为空且时间格式要为date'
         ];
     }
 }
