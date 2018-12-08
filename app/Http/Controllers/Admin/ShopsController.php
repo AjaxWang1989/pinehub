a7f44e56db0bb10327a66a878cad81fc69a1d9f3
ZoranWong <904110231@qq.com>
2018-12-08 17:12:01 +0800

    add merchant route

--- app/Http/Controllers/Admin/ShopsController.php
+++ app/Http/Controllers/Admin/ShopsController.php
@@ -305,7 +305,7 @@ class ShopsController extends Controller
     public function payQRCode(int $storeId, Request $request)
     {
         $url = buildUrl('web.mpPayment', '/pay/{storeId}', ['storeId' => $storeId]);
-        $code = app('qrcode')->format('png')->size($request->input('size', 128))->generate($url);
+        $code = app('qrcode')->format('png')->margin(2)->size($request->input('size', 128))->generate($url);
         return $this->response()->created()->setContent($code)->header('Content-Type', 'image/png');
     }
 
