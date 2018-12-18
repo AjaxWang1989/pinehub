984e8056a1ab6ec9042a9edad0ac23f90365b95e
ZoranWong <904110231@qq.com>
2018-12-18 15:06:48 +0800

    fix

--- app/Http/Controllers/MiniProgram/TicketController.php
+++ app/Http/Controllers/MiniProgram/TicketController.php
@@ -30,9 +30,9 @@ class TicketController extends Controller
         $tickets = $this->cardRepository->scopeQuery(function (Card $card) {
             return $card->whereNotIn('id', function (Builder $query) {
                 $query->from('cards')->select(DB::raw('cards.id  as id'))
-                    ->join('customer_ticket_cards', 'cards.id', '=', 'customer_ticket_cards.card_id');
-            })->where('customer_ticket_cards.customer_id', $this->mpUser()->id)
-                ->whereAppId(app(AppManager::class)->getAppId())
+                    ->join('customer_ticket_cards', 'cards.id', '=', 'customer_ticket_cards.card_id')
+                    ->where('customer_ticket_cards.customer_id', $this->mpUser()->id);
+            })->whereAppId(app(AppManager::class)->getAppId())
                 ->where(DB::raw('(issue_count - user_get_count)'), '>', 0);
         })->paginate($request->input('limit', PAGE_LIMIT));
         return $this->response()->paginator($tickets, new TicketTransformer());
