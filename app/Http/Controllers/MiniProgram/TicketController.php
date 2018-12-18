23a90ef8ddd7bf89200d8a4ad6e79b3176b41e41
ZoranWong <904110231@qq.com>
2018-12-18 14:57:50 +0800

    fix

--- app/Http/Controllers/MiniProgram/TicketController.php
+++ app/Http/Controllers/MiniProgram/TicketController.php
@@ -30,7 +30,7 @@ class TicketController extends Controller
         $tickets = $this->cardRepository->scopeQuery(function (Card $card) {
             return $card->whereNotIn('id', function (Builder $query) {
                 $query->select(DB::raw('cards.id  as id'))
-                    ->crossJoin('customer_ticket_cards', 'customer_ticket_cards.card_id', '=', 'cards.id')
+                    ->join('customer_ticket_cards', 'customer_ticket_cards.card_id', '=', 'cards.id')
                 ->where('customer_ticket_cards.customer_id', $this->mpUser()->id);
             })->whereAppId(app(AppManager::class)->getAppId())
                 ->where(DB::raw('(issue_count - user_get_count)'), '>', 0);
