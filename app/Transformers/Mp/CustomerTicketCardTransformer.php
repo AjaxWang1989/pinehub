68b12ebfcdf21d13c5472e2d71690d86d77d3c5d
ZoranWong <904110231@qq.com>
2019-01-07 14:34:49 +0800

    fix

--- app/Transformers/Mp/CustomerTicketCardTransformer.php
+++ app/Transformers/Mp/CustomerTicketCardTransformer.php
@@ -30,8 +30,8 @@ class CustomerTicketCardTransformer extends TransformerAbstract
             'card_type'   => isset($model->card->card_info['base_info']['date_info']['begin_timestamp']) && $model->card->card_info['base_info']['date_info']['begin_timestamp'] == 0
             ? $model->card->card_info['base_info']['date_info']['type'].'_0' : $model->card->card_info['base_info']['date_info']['type'].'_1',
 //            'record_count' => $model->card->recordCount,
-            'begin_timestamp' => isset($model->card->card_info['base_info']['date_info']['begin_timestamp']) ? date('Y-m-d H:i:s',$model->card->card_info['base_info']['date_info']['begin_timestamp']) : '',
-            'end_timestamp' => isset($model->card->card_info['base_info']['date_info']['end_timestamp']) ? date('Y-m-d H:i:s',$model->card->card_info['base_info']['date_info']['end_timestamp']) : '',
+            'begin_timestamp' => isset($model->card->card_info['base_info']['date_info']['begin_timestamp']) ? date('Y-m-d',$model->card->card_info['base_info']['date_info']['begin_timestamp']) : '',
+            'end_timestamp' => isset($model->card->card_info['base_info']['date_info']['end_timestamp']) ? date('Y-m-d',$model->card->card_info['base_info']['date_info']['end_timestamp']) : '',
             'fixed_begin_term' => isset($model->card->card_info['base_info']['date_info']['fixed_begin_term']) ? $model->card->card_info['base_info']['date_info']['fixed_begin_term'] : '',
             'fixed_term'       =>  isset($model->card->card_info['base_info']['date_info']['fixed_term']) ? $model->card->card_info['base_info']['date_info']['fixed_term'] : '',
         ];
