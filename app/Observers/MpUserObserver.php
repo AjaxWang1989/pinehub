e3ca03373e28a9a71d5b869d928b0e35728244dd
ZoranWong <904110231@qq.com>
2019-01-23 15:00:44 +0800

    fix ali pay

--- app/Observers/MpUserObserver.php
+++ app/Observers/MpUserObserver.php
@@ -14,7 +14,7 @@ use App\Services\AppManager;
 class MpUserObserver
 {
     public function creating(MpUser $mpUser) {
-        $mpUser->type = MpUser::TYPE;
+        //$mpUser->type = MpUser::TYPE;
         $mpUser->appId = app(AppManager::class)->getAppId();
         return $mpUser;
     }
