8bcc4f8b3c507d7b5d563dff71e9263fe4ea6187
ZoranWong <904110231@qq.com>
2019-01-25 22:16:23 +0800

    fix

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Entities\Shop;
+use Dingo\Api\Routing\Router;
 use Illuminate\Broadcasting\BroadcastController;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Broadcast;
@@ -17,8 +18,9 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot()
     {
 //        Broadcast::routes();
-        $this->app->make('api.router')->any('/broadcasting/auth', [
-            'as' => 'broadcasting.auth', 'version' => 'v1', 'uses' => BroadcastController::class.'@authenticate']);
+        $this->app->make('api.router')->version('v1', [], function (Router $router) {
+            $router->any('/broadcasting/auth', ['as' => 'broadcasting.auth', 'uses' => BroadcastController::class.'@authenticate']);
+        });
         Broadcast::channel('shop-{shopId}', function ($user, $shopId) {
             $shop = Shop::find($shopId);
             return $user->id === $shop->userId;
