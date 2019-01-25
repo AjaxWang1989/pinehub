f101d3f2e5424a0ec4c855b257d2de65b2b61d50
ZoranWong <904110231@qq.com>
2019-01-26 04:12:29 +0800

    fix

---
+++ laravel-echo-server.js
@@ -0,0 +1,21 @@
+const app   = require('express');
+const http  = require('http').Server(app);
+const io    = require('socket.io')(http);
+const Redis = require('ioredis');
+
+/*
+ * Your Redis connection
+ * @see https://www.npmjs.com/package/ioredis for more details
+ */
+const redis = new Redis('redis://47.100.255.104:6379/0');
+
+redis.psubscribe('*', function(err, count) {
+    //
+    console.log(err, count);
+});
+
+redis.on('pmessage', function(subscribed, channel, message) {
+    message = JSON.parse(message);
+    console.log("======message=======\n", message);
+    io.emit(channel + ':' + message.event, message.data);
+});
