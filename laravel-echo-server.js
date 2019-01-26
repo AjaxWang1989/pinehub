62ee498772df50bb270b847c87352c40838d22f0
ZoranWong <904110231@qq.com>
2019-01-26 10:54:12 +0800

    fix

--- laravel-echo-server.js
+++ laravel-echo-server.js
@@ -17,5 +17,5 @@ redis.psubscribe('*', function(err, count) {
 redis.on('pmessage', function(subscribed, channel, message) {
     message = JSON.parse(message);
     console.log("======message=======\n", message);
-    io.emit(channel + ':' + message.event, message.data);
+    //io.emit(channel + ':' + message.event, message.data);
 });
