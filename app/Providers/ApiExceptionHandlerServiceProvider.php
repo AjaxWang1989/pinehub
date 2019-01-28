be864925dab7ed88db95680c4649b0fa0d2ff9e0
ZoranWong <904110231@qq.com>
2019-01-28 17:24:09 +0800

    fix validate error

--- app/Providers/ApiExceptionHandlerServiceProvider.php
+++ app/Providers/ApiExceptionHandlerServiceProvider.php
@@ -34,6 +34,7 @@ class ApiExceptionHandlerServiceProvider extends ServiceProvider
                     ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept')
                     ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
                     ->header('Access-Control-Allow-Credentials', 'true');
+                var_dump($exception);
                 if(Request::method() === HTTP_METHOD_OPTIONS) {
                     return $responseSender->send();
                 }
