c5eb01fb6c29f333be035db89270515770a75964
ZoranWong <904110231@qq.com>
2019-02-28 13:01:51 +0800

    excel导出接口

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -140,10 +140,7 @@ class OrdersController extends Controller
         });
 
         $content = $excel->string();
-        return $this->response($content)->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Credentials', 'true');
+        return $this->response(['excel' => $content]);
 #
     }
     /**
