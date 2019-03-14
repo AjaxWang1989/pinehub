e4fe1e7e977adfd5cc8bfe109b554e713b16e1ab
ZoranWong <904110231@qq.com>
2019-03-14 09:50:13 +0800

    添加type/show字段到card_conditions表

--- database/migrations/2019_03_07_042208_create_card_conditions_table.php
+++ database/migrations/2019_03_07_042208_create_card_conditions_table.php
@@ -17,11 +17,12 @@ class CreateCardConditionsTable extends Migration
     {
         Schema::create('card_conditions', function (Blueprint $table) {
             $table->increments('id');
+            $table->enum('type', ['PUT', 'USE'])->comment('条件类型');
             $table->unsignedInteger('card_id')->comment('卡券ID');
             $table->boolean('paid')->default(false)->comment('支付可领取');
 //            $table->unsignedInteger('merchandise_id')->default(null)->comment('商品ID');
-            $table->json('valid_obj')->comment('作用对象["merchandises" => null,"shops" => null,"customers" => null]');
-//            $table->unsignedInteger('shop_id')->default(null)->comment('指定店铺id');
+            $table->json('valid_obj')->comment('作用对象["merchandises" => null, "shops" => null, "customers" => ["sex" => MALE/FEMALE, "tags" => null] ]');
+            $table->unsignedTinyInteger('show')->default(0)->comment('投放场景[0 （通用）1(聚合支付), 2（邻里优先）, 3（预订商场）]');
             $table->float('pre_payment_amount')->default(0)->comment('单笔支付满额领取');
             $table->unsignedTinyInteger('loop')->default(0)->comment('周期（天）');
             $table->unsignedTinyInteger('loop_order_num')->default(0)->comment('周期内购买多少单');
