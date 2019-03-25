81e282bb6931031718cfd484c64eafe53814ff13
katherine <1134052988@qq.com>
2019-03-25 18:08:00 +0800

    update

--- app/Transformers/Mp/StatusOrdersTransformer.php
+++ app/Transformers/Mp/StatusOrdersTransformer.php
@@ -7,6 +7,7 @@
  */
 
 namespace App\Transformers\Mp;
+
 use League\Fractal\TransformerAbstract;
 use App\Entities\OrderItem;
 use App\Entities\Order;
@@ -14,26 +15,28 @@ use App\Entities\Order;
 
 class StatusOrdersTransformer extends TransformerAbstract
 {
-    public function transform(Order $model){
+    public function transform(Order $model)
+    {
         $receiverAddress = json_decode($model->receiverAddress, true);
         return [
-            'id'      => $model->id,
-            'code'    => $model->code,
-            'type'    => $model->type,
+            'id' => $model->id,
+            'code' => $model->code,
+            'type' => $model->type,
             'pick_up_method' => $model->pickUpMethod,
-            'status'  => $model->status,
-            'receiver_name'    => $model->receiverName,
+            'status' => $model->status,
+            'receiver_name' => $model->receiverName,
             'receiver_address' => is_array($receiverAddress) ? $receiverAddress['receiver_address'] : $model->receiverAddress,
-            'build_num'        => is_array($receiverAddress) ? $receiverAddress['build_num'] : null,
-            'room_num'         => is_array($receiverAddress) ? $receiverAddress['room_num'] : null ,
-            'receiver_mobile'  => $model->receiverMobile,
-            'quantity'          => $model->merchandiseNum,
-            'total_amount'     => round($model->totalAmount,2),
-            'payment_amount'   => round($model->paymentAmount,2),
-            'shop_end_hour'    => isset($model->shop->end_at) ? $model->shop->end_at : null ,
-            'created_at'       => $model->createdAt->format('Y-m-d H:i:s'),
+            'build_num' => is_array($receiverAddress) ? $receiverAddress['build_num'] : null,
+            'room_num' => is_array($receiverAddress) ? $receiverAddress['room_num'] : null,
+            'receiver_mobile' => $model->receiverMobile,
+            'quantity' => $model->merchandiseNum,
+            'total_amount' => round($model->totalAmount, 2),
+            'payment_amount' => round($model->paymentAmount, 2),
+            'shop_end_hour' => isset($model->shop->end_at) ? $model->shop->end_at : null,
+            'created_at' => $model->createdAt->format('Y-m-d H:i:s'),
+            'paid_at' => $model->paidAt->format('Y-m-d H:i:s'),
             'order_item_merchandises' => $model->orderItems ? $model->orderItems->map(function (OrderItem $orderItem) {
-                $data  = $orderItem->only(['merchandise_name','sell_price','quality','total_amount','main_image']);
+                $data = $orderItem->only(['merchandise_name', 'sell_price', 'quality', 'total_amount', 'main_image']);
                 $data['sell_price'] = number_format($data['sell_price'], 2);
                 return $data;
             }) : null,
