1033cf9011d2f3ada901f5e9dc4a7dd8095fe2cd
ZoranWong <904110231@qq.com>
2019-04-04 11:01:34 +0800

    cross change

--- app/Http/Middleware/Cross.php
+++ app/Http/Middleware/Cross.php
@@ -31,13 +31,13 @@ class Cross
         $origin = $request->header('ORIGIN', '*');
         if (method_exists($response, 'header')) {
             $response->header('Access-Control-Allow-Origin', $origin);
-            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, Authorization, ProjectId,project_id');
+            $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept, Authorization, ProjectId, project_id');
             $response->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
             $response->header('Access-Control-Allow-Credentials', 'true');
             return $response;
         } else {
             header('Access-Control-Allow-Origin: ' . $origin);
-            header('Access-Control-Allow-Headers: Origin, Content-Type, Cookie, Accept, Authorization, ProjectId,project_id');
+            header('Access-Control-Allow-Headers: Origin, Content-Type, Cookie, Accept, Authorization, ProjectId, project_id');
             header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
             header('Access-Control-Allow-Credentials: true');
             return $response;
