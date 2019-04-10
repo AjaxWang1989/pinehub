03a1fb1e63196ace51c1ea1f2e163358a64cb1c0
katherine <1134052988@qq.com>
2019-04-10 09:06:15 +0800

    .

--- database/migrations/2019_04_02_011221_create_user_template_message_table.php
+++ database/migrations/2019_04_02_011221_create_user_template_message_table.php
@@ -17,7 +17,7 @@ class CreateUserTemplateMessageTable extends Migration
         Schema::create('user_template_messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('wx_app_id', 32)->comment('微信ID');
-            $table->unsignedTinyInteger('type')->default(UNDEFINED)->comment('模版类型');
+            $table->unsignedTinyInteger('type')->default(TEMPLATE_UNDEFINED)->comment('模版类型');
             $table->unsignedInteger('template_id')->comment('模版消息ID');
             $table->json('content')->comment('模版消息填充内容，参考微信文档');
             $table->timestamps();
