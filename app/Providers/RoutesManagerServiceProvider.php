86b441545a5d6402d3970a53ca1054bf05fbcf1b
wong1989 <skyandsunning@163.com>
2019-05-04 12:17:12 +0800

    fix

--- app/Providers/RoutesManagerServiceProvider.php
+++ app/Providers/RoutesManagerServiceProvider.php
@@ -142,6 +142,7 @@ class RoutesManagerServiceProvider extends ServiceProvider
                 if(isset($route['provider']) && $route['provider']) {
                     $this->app->register($route['provider']);
                 }
+                Log::debug('------- router class --------', $route);
                 if($auth){
                     $routes = new $route['router']($this->app, $route['version'], $route['namespace'], $prefix, $domain, $auth);
                 }else{
