dba1a040796dca0a97dd08faa0c2f057d4f4a42a
ZoranWong <904110231@qq.com>
2019-05-15 14:23:45 +0800

    微信支付

--- app/Ali/Oauth/Token.php
+++ app/Ali/Oauth/Token.php
@@ -14,6 +14,7 @@ use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
 use Payment\Common\Ali\AliBaseStrategy;
 use Payment\Common\PayException;
+use Payment\Utils\ArrayUtil;
 
 class Token extends AliBaseStrategy
 {
@@ -95,13 +96,37 @@ class Token extends AliBaseStrategy
         }
 
         // 验证签名，检查支付宝返回的数据
-//        $flag = $this->verifySign($body[$responseKey], $body['sign']);
-//        if (! $flag) {
-//            throw new PayException('支付宝返回数据被篡改。请检查网络是否安全！');
-//        }
+        $flag = $this->verifySign($body[$responseKey], $body['sign']);
+        if (! $flag) {
+            throw new PayException('支付宝返回数据被篡改。请检查网络是否安全！');
+        }
 
         // 这里可能带来不兼容问题。原先会检查code ，不正确时会抛出异常，而不是直接返回
         return $body[$responseKey];
     }
 
+    /**
+     * 检查支付宝数据 签名是否被篡改
+     * @param array $data
+     * @param string $sign  支付宝返回的签名结果
+     * @return bool
+     * @author helei
+     */
+    protected function verifySign(array $data, $sign)
+    {
+        $data = ArrayUtil::arraySort($data);
+        $preStr = \GuzzleHttp\json_encode($data, JSON_UNESCAPED_UNICODE);// 主要是为了解决中文问题
+
+        if ($this->config->signType === 'RSA') {// 使用RSA
+            $rsa = new RsaEncrypt($this->config->rsaAliPubKey);
+
+            return $rsa->rsaVerify($preStr, $sign);
+        } elseif ($this->config->signType === 'RSA2') {// 使用rsa2方式
+            $rsa = new Rsa2Encrypt($this->config->rsaAliPubKey);
+
+            return $rsa->rsaVerify($preStr, $sign);
+        } else {
+            return false;
+        }
+    }
 }
