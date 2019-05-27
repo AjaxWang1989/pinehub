f70ed8d4d3d5a8f5bf0106d9c4562e7ca23c8962
ZoranWong <904110231@qq.com>
2019-05-27 18:17:31 +0800

    添加订单类型

---
+++ app/Services/OrderStrategies/CreateOrderByMerchandise.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangzaron
+ * Date: 2019/5/20
+ * Time: 11:40 AM
+ */
+
+namespace App\Services\OrderStrategies;
+
+
+class CreateOrderByMerchandise extends CreateOrder
+{
+
+}
