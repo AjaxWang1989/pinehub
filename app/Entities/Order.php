c653fac37fa5e46b4165cd52c5abfed416135347
katherine <1134052988@qq.com>
2019-06-03 10:52:26 +0800

    improve

--- app/Entities/Order.php
+++ app/Entities/Order.php
@@ -78,6 +78,7 @@ use Prettus\Repository\Traits\TransformableTrait;
  * @property-read \App\Entities\Shop|null $receivingShopAddress
  * @property-read \App\Entities\Shop|null $shop
  * @property-read \App\Entities\Card $tickets
+ * @property-read \App\Entities\UserRechargeableCardConsumeRecord|null $consumeRecord
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Entities\Order newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Entities\Order query()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Entities\Order whereActivityId($value)
@@ -512,5 +513,10 @@ class Order extends Model implements Transformable
                 return '送货上门';
                 break;
         }
+    }
+
+    public function consumeRecord()
+    {
+        return $this->hasOne(UserRechargeableCardConsumeRecord::class, 'order_id');
     }
 }
