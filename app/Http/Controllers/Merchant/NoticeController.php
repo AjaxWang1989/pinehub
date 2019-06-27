5bdca6241d233306a17a27b03f15b6469d89ebe1
ZoranWong <904110231@qq.com>
2019-06-28 00:19:44 +0800

    fix

--- app/Http/Controllers/Merchant/NoticeController.php
+++ app/Http/Controllers/Merchant/NoticeController.php
@@ -65,23 +65,25 @@ class NoticeController extends Controller
             Log::debug('++++++++ payment message +++++++++', $message);
             return $message['voice'];
         });
-        $orders = app(OrderRepository::class)->findWhereIn('id', collect($messages)->map(function ($message) {
-            return $message['order_id'];
-        })->toArray());
+        $orders = collect([]);
+        if ($voices->count() > 0)
+            $orders = app(OrderRepository::class)->findWhereIn('id', collect($messages)->map(function ($message) {
+                return $message['order_id'];
+            })->toArray());
         if ($orders->count() > 0) {
-            Log::debug('------------- payment order count --------- '.$orders->count());
+            $orders = $orders->map(function (Order $order) {
+                return [
+                    'order_no' => $order->code,
+                    'pay_type' => $order->payTypeStr(),
+                    'pay_amount' => number_format($order->paymentAmount, 2),
+                    'paid_at' => $order->paidAt ? $order->paidAt->format('Y-m-d H:i:s') :
+                        \Illuminate\Support\Carbon::now()->format('Y-m-d H:i:s')
+                ];
+            });
         }
-        $orders = $orders->map(function (Order $order) {
-            return [
-                'order_no' => $order->code,
-                'pay_type' => $order->payTypeStr(),
-                'pay_amount' => number_format($order->paymentAmount, 2),
-                'paid_at' => $order->paidAt ? $order->paidAt->format('Y-m-d H:i:s') :
-                    \Illuminate\Support\Carbon::now()->format('Y-m-d H:i:s')
-            ];
-        });
 
-        return $this->response->item($shop, new ShopTransformer())
+
+        return $this->response->item($shop, new ShopTransformer($voices->count() > 0))
             ->addMeta('token', $tokenMeta)
             ->addMeta('voices', $voices)
             ->addMeta('orders', $orders);
