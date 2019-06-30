8978d33e2c834d5251bf1ab150e1284e8b82c2e6
ZoranWong <904110231@qq.com>
2019-06-30 23:39:28 +0800

    fix

--- redis_channel.js
+++ redis_channel.js
@@ -1,33 +1,30 @@
-var app   = require('express');
-var http  = require('http').Server(app);
-var io    = require('socket.io')(http);
-var Redis = require('ioredis');
-const PROTOCOL = 6002;
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
-/*
- * Your Redis connection
- * @see https://www.npmjs.com/package/ioredis for more details
- */
-var redis = new Redis('redis://redis:6379/0');
+const app = express();
 
+//initialize a simple http server
+const server = http.createServer(app);
 
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
 
-/*
- * Your broadcasting emitter
- */
-redis.on('message', function(channel, message){
-    // message = JSON.parse(message);
-    console.log('-------------------', channel, message);
-    // io.emit(channel+':'+message.event, message.payload);
+wss.on('connection', (ws) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
 });
 
-redis.on("error", function (err) {
-    console.log("Error " + err);
-});
-redis.subscribe('test-channel');
-/*
- * http server listen 6001
- */
-http.listen(PROTOCOL, function(){
-    console.log('Listen on 0.0.0.0:' + PROTOCOL);
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    console.log(`Server started on port ${server.address().port} :)`);
 });
